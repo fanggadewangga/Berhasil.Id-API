8df05b7b1c1baaa349a48c06ac3833d6c239a425
Fanggadewangga <fanggadewangga@gmail.com>
2022-07-09 15:12:58 +0700

    deploy to heroku

--- src/main/kotlin/com/raion/Application.kt
+++ src/main/kotlin/com/raion/Application.kt
@@ -3,11 +3,23 @@ package com.raion
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.raion.plugins.*
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
 import io.ktor.locations.*
 
 @KtorExperimentalLocationsAPI
 fun main() {
-    embeddedServer(Netty, port = System.getenv("PORT").toInt(), host = "localhost") {
+    embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
+        install(CORS) {
+            anyHost()
+            method(HttpMethod.Get)
+            method(HttpMethod.Post)
+            method(HttpMethod.Options)
+            method(HttpMethod.Put)
+            method(HttpMethod.Delete)
+            header(HttpHeaders.ContentType)
+        }
         configureRouting()
         configureSerialization()
     }.start(wait = true)
--- src/main/kotlin/com/raion/data/DatabaseFactory.kt
+++ src/main/kotlin/com/raion/data/DatabaseFactory.kt
@@ -7,15 +7,15 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.net.URI
 
 class DatabaseFactory {
 
     init {
         Database.connect(dataSource())
         transaction {
-            val tables = listOf<Table>(
+            val tables = listOf(
                 UserTable, UMKMTable, ReviewTable, PostTable, CommentTable
             )
             tables.forEach {
@@ -28,10 +28,16 @@ class DatabaseFactory {
         val config = HikariConfig()
         config.apply {
             driverClassName = System.getenv("JDBC_DRIVER")
-            jdbcUrl = System.getenv("DATABASE_URL")
+            //jdbcUrl = System.getenv("DATABASE_URL")
             maximumPoolSize = 6
             isAutoCommit = false
             transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+
+            val uri = URI(System.getenv("DATABASE_URL"))
+            val username = uri.userInfo.split(":").toTypedArray()[0]
+            val password = uri.userInfo.split(":").toTypedArray()[1]
+            jdbcUrl =
+                "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "?sslmode=require" + "&user=$username&password=$password"
         }
         return HikariDataSource(config)
     }
